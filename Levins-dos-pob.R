9173fbe988cca858e2126be8ed912421957d2f54
Gerardo Martín <gerardo@pop-os.localdomain>
2023-08-14 18:33:13 -0600

    SIR frec

--- Levins-dos-pob.R
+++ Levins-dos-pob.R
@@ -1,16 +1,16 @@
 # Parameters
 r <- 0.1       # Intrinsic growth rate
 K <- 100       # Carrying capacity
-e <- 0.05      # Extinction rate
-m <- 0.02      # Migration rate
+m1 <- 0.05      # Extinction rate
+m2 <- 0.02      # Migration rate
 
 # Initial population sizes in two patches
-N10 <- 20
-N20 <- 50
+N10 <- 2
+N20 <- 0
 
 # Time step and total time
 dt <- 0.1
-total_time <- 100
+total_time <- 200
 
 # Simulation loop
 time <- seq(0, total_time, by = dt)
@@ -20,8 +20,8 @@ N1[1] <- N10
 N2[1] <- N20
 
 for (i in 2:length(time)) {
-  dN1_dt <- r * N1[i - 1] * (1 - N1[i - 1] / K) - e * N1[i - 1] + m * (N2[i - 1] - N1[i - 1])
-  dN2_dt <- r * N2[i - 1] * (1 - N2[i - 1] / K) - e * N2[i - 1] + m * (N1[i - 1] - N2[i - 1])
+  dN1_dt <- r * N1[i - 1] * (1 - N1[i - 1] / K) - m1 * N1[i - 1] + m2 * (N2[i - 1] - N1[i - 1])
+  dN2_dt <- r * N2[i - 1] * (1 - N2[i - 1] / K) - m2 * N2[i - 1] + m1 * (N1[i - 1] - N2[i - 1])
   
   N1[i] <- N1[i - 1] + dN1_dt * dt
   N2[i] <- N2[i - 1] + dN2_dt * dt
@@ -32,6 +32,7 @@ for (i in 2:length(time)) {
 }
 
 # Plot the results
-plot(time, N1, type = "l", xlab = "Tiempo", ylab = "Tamaño de la población", col = "blue", main = "Modelo de Parches Conectados")
+plot(time, N1, type = "l", xlab = "Tiempo", ylab = "Tamaño de la población", col = "blue", main = "Modelo de Parches Conectados",
+     ylim = c(0, 100))
 lines(time, N2, col = "red")
 legend("topright", legend = c("Parche 1", "Parche 2"), col = c("blue", "red"), lty = 1)

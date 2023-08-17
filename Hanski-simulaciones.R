80df955ccd50f98d63cc901a38d7cbe37070b17b
Gerardo Martín <gerardo@pop-os.localdomain>
2023-08-17 17:11:02 -0600

    Examen

--- Hanski-simulaciones.R
+++ Hanski-simulaciones.R
@@ -11,9 +11,11 @@ area.cont <- rnorm(1, 200, 1)
 
 area.is <- (rnorm(10, 2, 2))^2
 
-plot(x, y, xlim = c(0,1), ylim = c(0,1),  pch = 20, cex = area.is/5)
-for(i in 1:10){lines(c(0, x[i]), c(0, y[i]), lty = 2, col = "grey")}
-points(0,0, pch = 20, col = "red", cex = area.cont/10)
+plot(x, y, xlim = c(0,1), ylim = c(0,1),  pch = 20, cex = area.is/5) #Gráfica de las islas
+for(i in 1:10){
+      lines(c(0, x[i]), c(0, y[i]), lty = 2, col = "grey")
+  } #Pintar las líneas
+points(0,0, pch = 20, col = "red", cex = area.cont/10) #Punto de la fuente continental
 
 dist <- sqrt(x^2 + y^2)
 
@@ -22,7 +24,7 @@ dist <- sqrt(x^2 + y^2)
 #Función para E
 E.p <- function(A, e=0.01, X=1.009){
   Ei <- ifelse(A > e^(1/X), e/(A^X), 1)
-  Ei }
+  Ei}
 
 x.is <- exp(rnorm(1, mean = 0.1, sd = 0.2))
 e <- exp(rnorm(1))
@@ -31,12 +33,10 @@ Ei <- E.p(A = area.is,
           e = e,
           X = x.is)
 
-
-
 #Función para Si
 C.p <- function(d, yprima, alfa, area){
   Sij <- exp(-alfa*d ) * area
-  Ci <- 1/(1 + (yprima/Sij)^2 )
+  Ci <- 1/(1 + (yprima/Sij)^2)
   Ci
 }
 
@@ -61,4 +61,17 @@ plot(x,y, pch = 20, cex = Ji*2,
 
 sim.sp <- c()
 for(i in 1:10){sim.sp[i] <- rbinom(1, 1, Ji[i])}
+
+s <- c()
+for(j in 1:100){
+for(i in 1:10){
+  s[i] <- rbinom(1, 1, Ji[i])
+}
+  sim.sp <- rbind(s, sim.sp)
+  
+  }
 sim.sp
+
+for(i in 1:100){
+  plot(x, y, cex = sim.sp[i, ], pch = 20)
+}
